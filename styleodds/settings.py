f5684559b358bb87499042e39574484262a75413
Harsh Gupta <150253719+harshgupta2125@users.noreply.github.com>
2025-07-21 22:02:33 +0530

    Update settings.py

--- styleodds/settings.py
+++ styleodds/settings.py
@@ -25,7 +25,7 @@ from dotenv import load_dotenv
 load_dotenv()
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['styleodds.onrender.com', '127.0.0.1']
 
